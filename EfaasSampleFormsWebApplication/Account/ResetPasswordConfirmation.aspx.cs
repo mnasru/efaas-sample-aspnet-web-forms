fea4384b00a9eea9ef4a66424897fc6848d168c4
Nasru <m.nasru@gmail.com>
2020-12-02 10:36:29 +0500

    Add project files.

---
+++ EfaasSampleFormsWebApplication/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace EfaasSampleFormsWebApplication.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ EfaasSampleFormsWebApplication/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EfaasSampleFormsWebApplication.Startup))]
+namespace EfaasSampleFormsWebApplication
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
